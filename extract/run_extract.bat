2e04bfd863819f876591e0d17bf16b029248ee52
P-aan0901 <22130004@st.hcmuaf.edu.vn>
2025-11-23 11:42:19 +0700

    update path

--- extract/run_extract.bat
+++ extract/run_extract.bat
@@ -1,4 +1,3 @@
 @echo off
-echo Script started at %date% %time% >> "C:\Users\ASUS VIVOBOOK\Desktop\debug_task_log.txt"
-"C:\Program Files\Python312\python.exe" "F:\2025-2026\DW\Project\extract\extract.py"
-pause
+"C:\Program Files\Python312\python.exe" ""D:\Data_warehousing\scripts\extract\extract.py""
+
